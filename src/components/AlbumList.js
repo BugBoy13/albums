cb5d3a86a2ea258e5d570ef9c0fa2b26c5527f1f
vardaan <vardaan13121997@gmail.com>
2021-06-20 02:58:44 +0530

    Getting album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,43 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+
+    state = {
+        albums: []
+    };
+
+    componentWillMount() {
+        axios.get('http://10.0.2.2:3000/albums')
+            .then(response => {
+                this.setState({
+                    albums: response.data
+                })
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => {
+            return (
+                <AlbumDetail
+                    key={album.title}
+                    album={album} />
+            )
+        })
+    }
+
     render() {
+
+        console.log(this.state);
+
         return (
             <View>
-                <Text>
-                    Album List
-                </Text>
+                {this.renderAlbums()}
             </View>
         )
     }
